aa26a03bfe596a55f49194a9485f3ae7346f4b54
LeeHsss <lii0730@icloud.com>
2021-07-27 20:32:34 +0900

    Initial Commit

---
+++ SwiftUI_Image_Tutorial/SwiftUI_Image_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Image_TutorialApp.swift
+//  SwiftUI_Image_Tutorial
+//
+//  Created by LeeHsss on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Image_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
